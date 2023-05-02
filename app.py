b0848c513bdd356d48c950e4b1ea691f5b8081ec
Affonso Henrique Ruiz Jurema <affonsohenriqueruiz@gmail.com>
2023-05-02 14:36:49 -0400

    feat: adding JWT

--- app.py
+++ app.py
@@ -3,6 +3,8 @@ from flask_smorest import Api
 from resources.store import blp as StoreBlueprint
 from resources.item import blp as ItemBlueprint
 from resources.tag import blp as TagBlueprint
+from resources.user import blp as UserBlueprint
+from flask_jwt_extended import JWTManager
 from db import db
 import models
 import os
@@ -21,12 +23,17 @@ def create_app(db_url=None):
     db.init_app(app)
     api = Api(app)
 
+    app.config["JWT_SECRET_KEY"] = "335843368045176340606476296251859817327"
+
+    jwt = JWTManager(app)
+
     with app.app_context():
         db.create_all()
 
     api.register_blueprint(StoreBlueprint)
     api.register_blueprint(ItemBlueprint)
     api.register_blueprint(TagBlueprint)
+    api.register_blueprint(UserBlueprint)
 
     return app
 
