907488dc309e37b6fa9929b975f7e8733f69c520
Affonso Henrique Ruiz Jurema <affonsohenriqueruiz@gmail.com>
2023-06-01 15:15:52 -0400

    feat: remove print

--- app.py
+++ app.py
@@ -24,7 +24,6 @@ def create_app(db_url=None):
     app.config["OPENAPI_SWAGGER_UI_URL"] = "https://cdn.jsdelivr.net/npm/swagger-ui-dist/"
     app.config["SQLALCHEMY_DATABASE_URI"] = db_url or os.getenv("DATABASE_URL", "sqlite:///data.db")
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-    print(app.config["SQLALCHEMY_DATABASE_URI"])
     db.init_app(app)
     migrate = Migrate(app, db)
     api = Api(app)
